adb4e2703068e3f720aa37869f6da06ef23bc54c
adhikari-amit <adhikariamit2001@gmail.com>
2024-01-09 19:00:23 +0530

    User Registration Route added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors  from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
+import userRoutes from './routes/users'
+
 mongoose.connect(process.env.DB_location as string)
 
 
@@ -12,10 +14,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 
-app.get('/api/test',async(req,res)=>{
-    res.json({message:"Hello TS"})
-})
-
+app.use('/api/users',userRoutes)
 
 app.listen(3005,()=>{
     console.log("running on port 3005")
